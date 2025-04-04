94e5c7dede2a7a01fd97d9971b440fe857a3e829
AntonetteAntolihao <AntonetteAntolihao@gmail.com>
2025-04-03 18:45:33 -0700

    added personality to counter app.

--- src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
+++ src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
@@ -1,4 +1,26 @@
 package aclcbukidnon.com.javafxactivity.controllers;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class CounterController {
+
+
+
+    @FXML
+    private Label labelCount;
+
+
+
+    @FXML
+    protected void onPlusClick() {
+
+    }
+
+    @FXML
+    protected void  onMinusClick() {
+
+    }
+
 }
